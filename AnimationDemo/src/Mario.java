ced6ecfa9a8cf02e9dd55cd8c48f4b455c301c72
rsrinivasan596 <rsrinivasan596@PR26.hhs.win.fuhsd.org>
2018-05-01 11:20:11 -0700

    set up the demo with the given files

---
+++ AnimationDemo/src/Mario.java
@@ -0,0 +1,31 @@
+
+
+import java.awt.*;
+import javax.swing.*;
+import java.awt.image.*;
+import java.util.*;
+
+public class Mario extends Sprite {
+
+	public static final int MARIO_WIDTH = 40;
+	public static final int MARIO_HEIGHT = 60;
+	
+	public Mario(int x, int y) {
+		super("mario.png", x, y, MARIO_WIDTH, MARIO_HEIGHT);
+	}
+
+	// METHODS
+	public void walk(int dir) {
+		// WALK!
+	}
+
+	public void jump() {
+		// JUMP!
+	}
+
+	public void act(ArrayList<Shape> obstacles) {
+		// FALL (and stop when a platform is hit)
+	}
+
+
+}
